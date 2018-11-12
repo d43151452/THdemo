6ee455adfc9c08fb90e2d03876c636070ac8e91c
张亚泽 <zhangyaze@czxy.com>
2018-11-12 21:32:16 +0800

    全部完成

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # THdemo
-天恒测试题demo
+请调整database.php的数据库配置
+以及将sql导入
+
+请执行以下命令运行环境
+php think run
+php think worker:server
+
--- application/http/Worker.php
+++ application/http/Worker.php
@@ -4,6 +4,8 @@ namespace app\http;
 
 use think\worker\Server;
 use app\index\model\Users;
+use app\index\model\Chat;
+use Db;
 
 class Worker extends Server
 {  
@@ -23,13 +25,62 @@ class Worker extends Server
     public function onMessage($connection,$data)
 	{
         $data = json_decode($data);
-        if($data['type'] == 'connect'){
-            $user = Users::where('token',$data['token'])->find();
-            
+        if($data->type == 'connect'){
+
+            $user = Users::where('token',$data->token)->find();
+            if(!$user){
+                $connection->send(json_encode(['type'=>'error','data'=>'用户不存在']));
+            }
+            $this->user['id'.$user->id] = $connection;
+            $connection->send(json_encode(['type'=>'connect', 'data'=>'连接成功']));
+            echo $user->id;
+
+        }else if($data->type == 'message'){
+            if($data->get_id == 0){
+                foreach($this->user as $k => $v){
+                    $v->send(json_encode([
+                        'type'=>'all',
+                        'send_id'=>$data->send_id,
+                        'send_name'=>$data->send_name,
+                        'message'=>$data->message
+                    ]));
+                }
+                Chat::create([
+                    'send_id'=>$data->send_id,
+                    'get_id'=>$data->get_id,
+                    'send_time'=>Db::raw('now()'),
+                    'message'=>$data->message,
+                    'send_name'=>$data->send_name,
+                ]);
+            }else if(!isset($this->user['id'.$data->get_id])){
+                $connection->send(json_encode(['type'=>'error','data'=>'该用户不在线']));
+            }else{
+                $connection->send(json_encode(['type'=>'msgsuccess']));
+                $this->user['id'.$data->get_id]
+                    ->send(json_encode([
+                        'type'=>'get',
+                        'send_id'=>$data->send_id,
+                        'send_name'=>$data->send_name,
+                        'message'=>$data->message
+                    ]));
+                Chat::create([
+                    'send_id'=>$data->send_id,
+                    'get_id'=>$data->get_id,
+                    'send_time'=>Db::raw('now()'),
+                    'message'=>$data->message,
+                    'send_name'=>$data->send_name,
+                ]);
+            }
         }
     }
     
     public function onClose($connection){
-
+        foreach ($this->user as $k => $v){
+            if($v == $connection){
+                unset($this->user[$k]);
+                echo '删除成功';
+                break;
+            }
+        }
     }
 }
